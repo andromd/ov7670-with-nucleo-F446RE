5475df5dad943ccae8f348c466b8a777c79ec60f
_emacs <eruedav1600@alumno.ipn.mx>
2019-05-27 22:55:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include "mbed.h"
 #include "global.h"
 #include "FastPWM.h"
-
 #include "OV7670.h"
 #include "ov7670sreg.h"
-
 #include "ILI9341.h"
 #include "Arial12x12.h"
 #include "Arial24x23.h"
@@ -16,7 +14,6 @@
 #define WRTFT     PA_1
 #define RDTFT     PA_0
 
-
 #define PCLK    PC_10 //camera Pixel clock 
 #define HREF    PC_11 //camera Href
 #define VSYNC   PC_12 // camera Vsync
@@ -24,7 +21,6 @@
 #define I2C_CLK PB_10 // Camera SCCB port clock
 #define RESET   PB_12 // camera reset
 #define XCLK    PA_10 // camera system clock
-
 
 #define mD0     PC_2//camera Data
 #define mD1     PC_3//camera Data
@@ -35,18 +31,11 @@
 #define mD6     PC_8 // camera Data
 #define mD7     PC_9 //camera Data
 
-
 #define SCSD    PB_13   // clk SD
 #define MISD    PB_14   // miso SD
 #define MOSD    PB_15   // mosi SD
 
-
-// to check his use
-#define PD2 PD_2 /: CS SD
-
 #define QQVGA   19200          //160*120
-
-
 
 /*------------------------------------------------------------------------------
 -----                      GLOBAL INSTANCES
@@ -54,7 +43,7 @@
 ------------------------------------------------------------------------------*/
 DigitalOut myled(LED1);
 
-// communication serial
+// serial communication
 Serial pc(USBTX, USBRX);
 
 PinName buspins[8] = {D11, D10, D8, D7, D5, D4, PB_2, PA_12};
@@ -64,15 +53,11 @@ ILI9341 TFT(BUS_8, buspins, PB_0, PC_1, PA_4, PA_1, PA_0, "TFT", 240, 320);
 int mask =0x1FFC;
 OV7670 _OV7670(I2C_D,I2C_CLK,XCLK,PortC,mask,RESET);
 
-
-
 /*------------------------------------------------------------------------------
 ---                          GLOBAL VARIABLES
 ---
 ------------------------------------------------------------------------------*/
 Timer Time;
-
-
 char desfile[25];
 char patfile[25];
 char filename[25];
@@ -89,9 +74,7 @@ float meas0,measold0,meas1,measold1,meas2,measold2, meas3, measold3;
 int volatile statc=0;
 bool volatile captur =false;
 
- 
-void capturecycle(const char des[])
-{
+void capturecycle(const char des[]) {
     strcpy( desfile,des);
     if (statc==0||statc==1) {
         _OV7670.CaptureNext() ;
@@ -106,7 +89,6 @@ void capturecycle(const char des[])
     }
     
 } 
- 
  
 int main() {
     myled = 0;
@@ -125,9 +107,7 @@ int main() {
     _OV7670.Init("RGB", QQVGA);
     pc.printf("Hello World !\r\n");
     
-    
     while (1) {
         capturecycle("picture_test");
-    }
-        
+    }    
 }
